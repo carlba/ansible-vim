dfb19b155f5a71d4fafe9ad27acfc68eaa655410
Carl Bäckström <genzorg@gmail.com>
2021-03-28 22:34:48 +0200

    update color scheme to dark

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
         name='ansible-vim',
         version='0.0.1',
         packages=[''],
-        scripts=[
-            'scripts/role-test',
-            'scripts/role-update'
-        ],
         url='https://github.com/carlba/ansible-vim',
         license='MIT',
         author='cbackstrom',
